7c06a199e2c37df476dc4b56984642066dc45126
John Kim <abcdefghijohnk@gmail.com>
2020-12-14 09:57:45 -0700

    Create foods index view

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,14 +1,14 @@
 class FoodsController < ApplicationController
   def index
-    food = params[:q]
+    @food = params[:q]
 
     conn = Faraday.new(url: "https://api.nal.usda.gov") do |f|
       f.params[:api_key] = ENV['FOOD_SEARCH_API_KEY']
     end
 
-    response = conn.get("/fdc/v1/foods/search?query=#{food}")
-    
+    response = conn.get("/fdc/v1/foods/search?query=#{@food}")
+
     json = JSON.parse(response.body, symbolize_names: true)
-    @foods = json[:results]
+    @foods = json[:foods]
   end
 end
